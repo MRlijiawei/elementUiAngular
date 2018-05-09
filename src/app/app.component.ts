8ca64bdbeecd048ca5ee2617e3756d0bc7e3c0c7
李家伟 <1341924634@qq.com>
2018-05-09 11:24:37 +0800

    20180509 李家伟 elementui-angular组件学习

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ElModule } from "element-angular";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  title = 'element ui angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ElModule } from "element-angular";
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ElModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
